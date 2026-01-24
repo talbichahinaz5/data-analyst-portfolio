759f47fafc9c375e340527beb6f830346a57942a
talbichahinaz5 <talbichahinas92@gmail.com>
2026-01-24 01:35:10 +0100

    Create queries.sql

---
+++ sql/queries.sql
@@ -0,0 +1,35 @@
+-- 1) Ventes et profit par catégorie
+SELECT
+  Category,
+  ROUND(SUM(Sales), 2) AS total_sales,
+  ROUND(SUM(Profit), 2) AS total_profit,
+  ROUND(SUM(Profit) / NULLIF(SUM(Sales),0), 4) AS profit_margin
+FROM orders
+GROUP BY Category
+ORDER BY total_sales DESC;
+
+-- 2) Top 10 sous-catégories par profit
+SELECT
+  "Sub-Category" AS sub_category,
+  ROUND(SUM(Profit), 2) AS total_profit
+FROM orders
+GROUP BY "Sub-Category"
+ORDER BY total_profit DESC
+LIMIT 10;
+
+-- 3) Ventes par mois
+SELECT
+  substr("Order Date", 7, 4) || '-' || substr("Order Date", 4, 2) AS year_month,
+  ROUND(SUM(Sales), 2) AS total_sales
+FROM orders
+GROUP BY year_month
+ORDER BY year_month;
+
+-- 4) Top 10 clients par ventes
+SELECT
+  "Customer Name" AS customer_name,
+  ROUND(SUM(Sales), 2) AS total_sales
+FROM orders
+GROUP BY "Customer Name"
+ORDER BY total_sales DESC
+LIMIT 10;
